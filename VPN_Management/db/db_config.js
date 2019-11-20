3c44d96dbd9e4bf23ef8cd6a89110c7e641251bb
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2019-11-19 17:24:23 -0800

    Created Amazon RDS DB instance

--- VPN_Management/db/db_config.js
+++ VPN_Management/db/db_config.js
@@ -1,7 +1,7 @@
 const DB = {
-	host: "localhost",
-	user: "root",
-	password: "password",
+	host: "vpn-management.cdrrldhunyw7.us-east-2.rds.amazonaws.com",
+	user: "admin",
+	password: "password1234",
 	database:"vpn_management"
 }
 
