9b4f6594133b718d0330850012df5e132f5b9910
Priyanshi Jajoo <48080485+priyanshi9692@users.noreply.github.com>
2019-11-23 09:55:59 -0800

    Update db_config.js

--- VPN_Management/db/db_config.js
+++ VPN_Management/db/db_config.js
@@ -1,7 +1,7 @@
 const DB = {
 	host: "vpn-management.cdrrldhunyw7.us-east-2.rds.amazonaws.com",
 	user: "admin",
-	password: "password1234",
+	password: "xxxxxxxxxxx",
 	database:"vpn_management"
 }
 
